825ec842a0e16a40ec65578cc1ba56f3c1a00de5
tscmoo <the.dengus@gmail.com>
2020-11-02 15:29:34 +0100

    Revert "butcher messages"
    This reverts commit c12ebe87aac5fca4ef4f5255e091bb082e6d2f9e.

--- tensorpipe/channel/channel.h
+++ tensorpipe/channel/channel.h
@@ -45,8 +45,8 @@
 namespace tensorpipe {
 namespace channel {
 
-//using TDescriptor = std::string;
-using TDescriptorCallback = Function<void(const Error&)>;
+using TDescriptor = std::string;
+using TDescriptorCallback = Function<void(const Error&, TDescriptor)>;
 using TSendCallback = Function<void(const Error&)>;
 using TRecvCallback = Function<void(const Error&)>;
 
@@ -62,6 +62,7 @@ class Channel {
 
   // Receive memory region from peer.
   virtual void recv(
+      TDescriptor descriptor,
       TBuffer buffer,
       TRecvCallback callback) = 0;
 
